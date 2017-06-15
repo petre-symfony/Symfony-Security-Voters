ca6f5d0e3b58e7a25f40f913c951d4986d8a1257
petrero <petrero@gmail.com>
2017-06-15 19:36:48 +0300

    Filling in CookieVoter First, in getSupportedClasses, were going to return the DeliciousCookie class string. This tells Symfony that when we pass a DeliciousCookie object to isGranted, our voter should be called. We’ll do the same thing in getSupportedAttributes and we’ll return an array with the NOM string. This tells Symfony that when we pass NOM to isGranted that our voter should be called. Whenever both of these functions return true, the  function at the bottom of this class is going to be called.
    For now I’ll just use the glorious var_dump to print the attribute object and user and I’m going to put a die after that.

--- src/AppBundle/Security/CookieVoter.php
+++ src/AppBundle/Security/CookieVoter.php
@@ -5,15 +5,15 @@ use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
 
 class CookieVoter extends AbstractVoter {
   protected function getSupportedAttributes() {
-    
+    return array('NOM');
   }
 
   protected function getSupportedClasses() {
-    
+    return array('AppBundle\Entity\DeliciousCookie');  
   }
 
   protected function isGranted($attribute, $object, $user = null) {
-    
+    var_dump($attribute, $object, $user);die;  
   }
 
 }
