12b738ca60f5e30f501fe7261c43ea528b42f272
petrero <petrero@gmail.com>
2017-06-15 18:57:51 +0300

    Creating a Voter   So the goal of a voter is to allow us to centralize that logic so we only have it in one spot. I’ll create a Security directory which is purely for organization and then put a CookieVoter inside of it. I’m using Symfony 2.6 for this project, which comes with a fantastic new AbstractVoter class which I’m going to use. If you’re using Symfony 2.5 or lower, you can actually find this class on the internet and just use it in your project today. Just update the namespace to match your project and then extend it. This class doesn’t have any external dependencies so it’s going to work just fine in whatever Symfony version you have.
      So I’ll extend it and then use a really nice feature in PHPstorm to tell me the three abstract methods that I need to fill in

---
+++ src/AppBundle/Security/CookieVoter.php
@@ -0,0 +1,20 @@
+<?php
+namespace AppBundle\Security\CookieVoter;
+
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+
+class CookieVoter extends AbstractVoter {
+  protected function getSupportedAttributes() {
+    
+  }
+
+  protected function getSupportedClasses() {
+    
+  }
+
+  protected function isGranted($attribute, $object, $user = null) {
+    
+  }
+
+//put your code here
+}
